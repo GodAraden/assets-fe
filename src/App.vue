02e41b0105c53dc60768d48bb61bf45297650d49
GodAraden <me@araden.top>
2024-02-15 21:52:30 +0800

    chore: 代码格式化

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,14 @@
     <!-- 文件上传框 -->
     <label for="asset">文件</label> <input type="file" ref="input" id="asset" />
     <!-- 密钥输入框 -->
-    <label for="key">密钥</label> <input type="password" v-model="key" id="key" />
+    <label for="key">密钥</label>
+    <input type="password" v-model="key" id="key" />
     <!-- 上传后是否保留原文件名，即不加时间戳 -->
-    <label for="hold">保留原名</label> <input type="checkbox" v-model="hold" id="hold" />
+    <label for="hold">保留原名</label>
+    <input type="checkbox" v-model="hold" id="hold" />
     <!-- 是否在文件名前加上 shared- 前缀 -->
-    <label for="shared">共享文件</label> <input type="checkbox" v-model="shared" id="shared" />
+    <label for="shared">共享文件</label>
+    <input type="checkbox" v-model="shared" id="shared" />
   </div>
 
   <button style="margin: 32px" @click="onUpload">上传</button> <br />
@@ -18,66 +21,71 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { MD5 } from 'crypto-js'
-import axios from 'axios'
+import { ref } from "vue";
+import { MD5 } from "crypto-js";
+import axios from "axios";
 
-const defaultTip = '上传成功后，这里会展示用于访问文件的文件名，点击复制完整URL 🙌'
-const successTip = '复制成功 🎇'
+const defaultTip =
+  "上传成功后，这里会展示用于访问文件的文件名，点击复制完整URL 🙌";
+const successTip = "复制成功 🎇";
 
-const key = ref('')
-const hold = ref(true)
-const shared = ref(false)
-const tip = ref(defaultTip)
-const input = ref<HTMLInputElement>()
+const key = ref("");
+const hold = ref(true);
+const shared = ref(false);
+const tip = ref(defaultTip);
+const input = ref<HTMLInputElement>();
 
-let timer = null
+let timer = null;
 
 // 判断提示信息是否是文件名，非文件名的提示内容会以一个 emoji 结尾
 function isTipFilename() {
-  const notFilenameSuffix = ['🙌', '🎇', '🙀']
-  let res = true
+  const notFilenameSuffix = ["🙌", "🎇", "🙀"];
+  let res = true;
   for (const emoji of notFilenameSuffix) {
-    res = res && !tip.value.endsWith(emoji)
+    res = res && !tip.value.endsWith(emoji);
   }
-  return res
+  return res;
 }
 
 // 复制资源完整访问 url 到剪切板
 async function onCopy() {
   if (isTipFilename()) {
-    const domain = import.meta.env.PROD ? 'https://assets.araden.top/' : 'http://localhost:10086/'
-    await navigator.clipboard.writeText(domain + tip.value)
-    tip.value = successTip
+    const domain = import.meta.env.PROD
+      ? "https://assets.araden.top/"
+      : "http://localhost:10086/";
+    await navigator.clipboard.writeText(domain + tip.value);
+    tip.value = successTip;
   }
 }
 
 // 上传资源，资源的命名单独传一下
 async function upLoadAsset(asset: File) {
-  const formData = new FormData()
-  formData.append('asset', asset)
-  formData.append('hold', hold.value ? 'hold' : '')
-  formData.append('shared', shared.value ? 'shared' : '')
-  formData.append('name', asset.name)
+  const formData = new FormData();
+  formData.append("asset", asset);
+  formData.append("hold", hold.value ? "hold" : "");
+  formData.append("shared", shared.value ? "shared" : "");
+  formData.append("name", asset.name);
   try {
-    const url = import.meta.env.PROD ? 'https://assets.araden.top/' : '/api/'
+    const url = import.meta.env.PROD ? "https://assets.araden.top/" : "/api/";
     const { data } = await axios.post<string>(url, formData, {
       headers: {
-        'upload-assets-key': MD5(key.value + new Date().toLocaleDateString('zh-CN')).toString()
-      }
-    })
-    return data
+        "upload-assets-key": MD5(
+          key.value + new Date().toLocaleDateString("zh-CN"),
+        ).toString(),
+      },
+    });
+    return data;
   } catch (error) {
-    return `${error.response?.data.statusCode || '444'}: ${error.response?.data.message || '未知错误'} 🙀`
+    return `${error.response?.data.statusCode || "444"}: ${error.response?.data.message || "未知错误"} 🙀`;
   }
 }
 
 // 上传按钮点击事件处理
 async function onUpload() {
-  const file = input.value.files[0]
-  if (!file) return
-  tip.value = await upLoadAsset(file)
-  clearTimeout(timer)
-  timer = setTimeout(() => (tip.value = defaultTip), 23300)
+  const file = input.value.files[0];
+  if (!file) return;
+  tip.value = await upLoadAsset(file);
+  clearTimeout(timer);
+  timer = setTimeout(() => (tip.value = defaultTip), 23300);
 }
 </script>
